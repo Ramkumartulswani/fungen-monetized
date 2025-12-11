1061aee35954b47b81e9824fd093336f55155a06
Ramkumartulswani <33463520+Ramkumartulswani@users.noreply.github.com>
2025-12-11 16:03:31 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/ramkumar/fungen/MainActivity.java
@@ -0,0 +1,10 @@
+package com.ramkumar.fungen;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "FunGen";
+    }
+}
