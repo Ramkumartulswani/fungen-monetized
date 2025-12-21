f0a20564e2eaf0d0b14567e1841c14320a91e11d
Ramkumartulswani <33463520+Ramkumartulswani@users.noreply.github.com>
2025-12-21 17:06:24 +0000

    modified Mainactivity.java for firebased

--- FunGenNew/App.tsx
+++ FunGenNew/App.tsx
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import { StatsProvider } from './app/context/StatsContext';
 import { getFeatureFlags } from './app/utils/featureFlags';
+import mobileAds from 'react-native-google-mobile-ads';
+
 
 export default function App() {
   const [booted, setBooted] = useState(false);
