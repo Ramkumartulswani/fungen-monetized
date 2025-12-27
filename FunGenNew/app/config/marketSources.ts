f500a18a043536b2fd531075a8607e0339164b68
Ramkumartulswani <33463520+Ramkumartulswani@users.noreply.github.com>
2025-12-27 13:38:03 +0530

    Revert "Fungen 18 12 25 bef27 commit"

--- FunGenNew/app/config/marketSources.ts
+++
@@ -1,6 +0,0 @@
-export const MARKET_URLS = {
-  NIFTY:
-    'https://drive.google.com/uc?export=download&id=1t9fYO6ry9igdt3DZqlBqakMArBA4CdUK',
-  BANKNIFTY:
-    'https://drive.google.com/uc?export=download&id=1Yj0AtywQaR-RW0ofrOpw7p8Yi1S66WVa',
-};
