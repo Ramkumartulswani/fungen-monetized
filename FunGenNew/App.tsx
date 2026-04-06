639f16607d5acdbc0f1a6b0d9e9ee4da35acdd93
Developer <developer@example.com>
2026-04-06 23:12:10 +0530

    Remove Google Mobile Ads (incompatible with this React Native version)

--- FunGenNew/App.tsx
+++ FunGenNew/App.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/navigation/AppNavigator';
 import { StatsProvider } from './app/context/StatsContext';
 import { ErrorBoundary } from './app/components/ErrorBoundary';
-import mobileAds from 'react-native-google-mobile-ads';
-
-mobileAds()
-  .initialize()
-  .then(adapterStatuses => {
-    console.log('Ads initialized:', adapterStatuses);
-  });
 
 export default function App() {
   return (
